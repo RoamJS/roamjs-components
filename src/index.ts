394f8e5485f4e100a23b18551ccd49d36b0e6727
David Vargas <dvargas92495@gmail.com>
2021-06-07 16:02:32 -0700

    Supporting several helper utilities from Service Components

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,22 @@ export { createConfigObserver } from "./ConfigPage";
 export { default as Description } from "./Description";
 export { default as MenuItemSelect } from "./MenuItemSelect";
 export {
+  TOKEN_STAGE as SERVICE_TOKEN_STAGE,
+  MainStage as WrapServiceMainStage,
+  ServiceDashboard,
+  StageContent,
+  useAuthenticatedAxiosDelete as useAuthenticatedDelete,
+  useAuthenticatedAxiosPost as useAuthenticatedPost,
+  useAuthenticatedAxiosGet as useAuthenticatedGet,
+  useAuthenticatedAxiosPut as useAuthenticatedPut,
+  useNextStage as useServiceNextStage,
+  usePageUid as useServicePageUid,
+  useField as useServiceField,
+  NextButton as ServiceNextButton,
+  useSetMetadata as useServiceSetMetadata,
+  useGetMetadata as useServiceGetMetadata,
+} from './ServiceComponents';
+export {
   default as WarningToast,
   render as renderWarningToast,
 } from "./WarningToast";

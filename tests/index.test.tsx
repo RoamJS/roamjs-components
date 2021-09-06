18d1889cbe71b446f4e6e008aa108e9caca3ed54
David Vargas <dvargas92495@gmail.com>
2021-09-05 22:07:57 -0400

    generate uid test

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -7,6 +7,7 @@ test("Renders Package", () => {
   // @ts-ignore Not setting this whole object get out. Might one day build roam-jest
   window.roamAlphaAPI = {};
   window.roamAlphaAPI.q = () => [["exists"]];
+  window.roamAlphaAPI.util.generateUID = () => 'abcdefghi';
   render(<div className="roam-body" />);
   createConfigObserver({ title: "roam/js/test", config: { tabs: [] } });
 });

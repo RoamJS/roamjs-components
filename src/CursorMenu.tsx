dc1876095e3f0b9072b60705a7cad6b352ea5fc0
David Vargas <dvargas92495@gmail.com>
2021-09-14 13:16:31 -0400

    onSelect itemsById

--- src/CursorMenu.tsx
+++ src/CursorMenu.tsx
@@ -234,7 +234,7 @@ const CursorMenu = <T extends Record<string, string>>({
                     )}
                   active={i === activeIndex}
                   onMouseEnter={() => setActiveIndex(i)}
-                  onClick={() => onSelect(id)}
+                  onClick={() => onSelect(itemsById[id])}
                 />
               );
             })

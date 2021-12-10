082eb3c8cee4f00cb5b54bfe8eb696804362661a
David Vargas <dvargas92495@gmail.com>
2021-12-10 12:16:57 -0500

    In Block Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,32 @@
+import { Spinner, SpinnerSize } from "@blueprintjs/core";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Loading = () => {
+  return <Spinner size={SpinnerSize.SMALL} />;
+};
+
+export const renderLoading = (targetUid: string) => {
+  const reactParent = document.createElement("div");
+  reactParent.style.position = "absolute";
+  reactParent.style.top = "0";
+  reactParent.style.right = "0";
+  const parent =
+    document.querySelector<HTMLDivElement>(
+      `.rm-block__input[id$="${targetUid}"]`
+    )?.parentElement || document.querySelector(".roam-article");
+  if (parent) {
+    parent.appendChild(reactParent);
+    ReactDOM.render(<Loading />, reactParent);
+    return () => {
+      ReactDOM.unmountComponentAtNode(reactParent);
+      reactParent.remove();
+    };
+  } else {
+    return () => {
+      // no parent found
+    };
+  }
+};
+
+export default Loading;

bf0128e6519c64874ecce0b2d13b5de8e80b67f1
David Vargas <dvargas92495@gmail.com>
2021-12-15 16:20:38 -0500

    createPage takes an uid input

--- src/writes/createPage.ts
+++ src/writes/createPage.ts
@@ -7,13 +7,14 @@ import parseRoamDate from "../date/parseRoamDate";
 const createPage = ({
   title,
   tree = [],
+  uid = DAILY_NOTE_PAGE_TITLE_REGEX.test(title)
+    ? toRoamDateUid(parseRoamDate(title))
+    : window.roamAlphaAPI.util.generateUID(),
 }: {
   title: string;
   tree?: InputTextNode[];
+  uid?: string;
 }): string => {
-  const uid = DAILY_NOTE_PAGE_TITLE_REGEX.test(title)
-    ? toRoamDateUid(parseRoamDate(title))
-    : window.roamAlphaAPI.util.generateUID();
   window.roamAlphaAPI.createPage({ page: { title, uid } });
   tree.forEach((node, order) => createBlock({ node, parentUid: uid, order }));
   return uid;

410f4cdc707d9d4c54217cf576194c6b03f40948
David Vargas <dvargas92495@gmail.com>
2022-01-13 19:00:51 -0600

    getRoamJsUser improvements

--- src/backend/getRoamJSUser.ts
+++ src/backend/getRoamJSUser.ts
@@ -1,33 +1,35 @@
 import axios from "axios";
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { APIGatewayProxyResult, APIGatewayProxyHandler } from "aws-lambda";
 import headers from "./headers";
+
+type RoamJSUser = { email: string; [k: string]: unknown };
 
 const getRoamJSUser = (token: string, extensionId: string) =>
   axios
-    .get(`https://lambda.roamjs.com/user`, {
+    .get<RoamJSUser>(`https://lambda.roamjs.com/user`, {
       headers: {
         Authorization: process.env.ROAMJS_DEVELOPER_TOKEN,
         "x-roamjs-token": token,
         "x-roamjs-service": extensionId,
       },
     })
-    .catch((e) => ({
-      statusCode: 401,
-      body: e.response?.data,
-      headers,
-    }));
+    .then((r) => r.data);
 
-export const awsGetRoamJSUser = (
-  event: APIGatewayProxyEvent,
-  extensionId: string
-) =>
-  getRoamJSUser(
-    event.headers.Authorization || event.headers.authorization || "",
-    extensionId
-  ).catch((e) => ({
-    statusCode: 401,
-    body: e.response?.data,
-    headers,
-  }));
+export const awsGetRoamJSUser =
+  (
+    handler: (u: RoamJSUser) => APIGatewayProxyResult,
+    extensionId = process.env.ROAMJS_EXTENSION_ID || ""
+  ): APIGatewayProxyHandler =>
+  (event) =>
+    getRoamJSUser(
+      event.headers.Authorization || event.headers.authorization || "",
+      extensionId
+    )
+      .then(handler)
+      .catch((e) => ({
+        statusCode: 401,
+        body: e.response?.data,
+        headers,
+      }));
 
 export default getRoamJSUser;

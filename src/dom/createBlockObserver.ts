efee8b841161971edc5bba8453987ae51eb42cda
David Vargas <dvargas92495@gmail.com>
2022-01-24 14:52:10 -0600

    Create Block Observer accepts unload callbacks

--- src/dom/createBlockObserver.ts
+++ src/dom/createBlockObserver.ts
@@ -1,22 +1,44 @@
 import createHTMLObserver from "./createHTMLObserver";
 
 const createBlockObserver = (
-  blockCallback: (b: HTMLDivElement) => void,
+  blockCallbackOrConfig:
+    | ((b: HTMLDivElement) => void)
+    | {
+        onBlockLoad?: (b: HTMLDivElement) => void;
+        onBlockUnload?: (b: HTMLDivElement) => void;
+        onBlockRefLoad?: (b: HTMLSpanElement) => void;
+        onBlockRefUnload?: (b: HTMLSpanElement) => void;
+      },
   blockRefCallback?: (b: HTMLSpanElement) => void
 ): MutationObserver[] => {
   const blockObserver = createHTMLObserver({
-    callback: (e) => blockCallback(e as HTMLDivElement),
+    callback: (e) =>
+      typeof blockCallbackOrConfig === "function"
+        ? blockCallbackOrConfig(e as HTMLDivElement)
+        : blockCallbackOrConfig.onBlockLoad?.(e as HTMLDivElement),
+    removeCallback: (e) =>
+      typeof blockCallbackOrConfig === "object" &&
+      blockCallbackOrConfig.onBlockUnload?.(e as HTMLDivElement),
     tag: "DIV",
     className: "roam-block",
   });
   if (blockRefCallback) {
-    return [blockObserver, createHTMLObserver({
-      callback: blockRefCallback,
-      tag: "SPAN",
-      className: "rm-block-ref",
-    })];
+    return [
+      blockObserver,
+      createHTMLObserver({
+        callback: (e) =>
+          typeof blockCallbackOrConfig === "object"
+            ? blockCallbackOrConfig.onBlockRefLoad?.(e as HTMLSpanElement)
+            : blockRefCallback?.(e as HTMLSpanElement),
+        removeCallback: (e) =>
+          typeof blockCallbackOrConfig === "object" &&
+          blockCallbackOrConfig.onBlockRefUnload?.(e as HTMLSpanElement),
+        tag: "SPAN",
+        className: "rm-block-ref",
+      }),
+    ];
   }
-  return [blockObserver]
+  return [blockObserver];
 };
 
 export default createBlockObserver;

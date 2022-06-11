5e162b474f35fd492317ba1a7abe38426bac6080
David Vargas <dvargas92495@gmail.com>
2022-06-11 19:08:14 -0400

    rever util - test extension needs it

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,25 @@
-throw new Error(
-  `Do not import from roamjs-components/util directly. Instead import the *method* you need directly. For example: 
-    
-import runExtension from "roamjs-components/util/runExtension;"`
-);
+export { default as addInputSetting } from "./addInputSetting";
+export { default as apiDelete} from "./apiDelete";
+export { default as apiGet} from "./apiGet";
+export { default as apiPost} from "./apiPost";
+export { default as apiPut} from "./apiPut";
+export { default as createOverlayRender } from "./createOverlayRender";
+export { default as createTagRegex } from "./createTagRegex";
+export { default as extractRef } from "./extractRef";
+export { default as extractTag } from "./extractTag";
+export { default as extensionDeprecatedWarning } from "./extensionDeprecatedWarning";
+export { default as getGraph } from "./getGraph";
+export { default as getOauth } from "./getOauth";
+export { default as getOauthAccounts } from "./getOauthAccounts";
+export { default as getRenderRoot } from "./getRenderRoot";
+export { default as getSettingIntFromTree } from "./getSettingIntFromTree";
+export { default as getSettingValueFromTree } from "./getSettingValueFromTree";
+export { default as getSettingValuesFromTree } from "./getSettingValuesFromTree";
+export { default as getSubTree } from "./getSubTree";
+export { default as getWorkerClient } from "./getWorkerClient";
+export { default as idToTitle } from "./idToTitle";
+export { default as isControl } from "./isControl";
+export { default as runExtension } from "./runExtension";
+export { default as setInputSetting } from "./setInputSetting";
+export { default as setInputSettings } from "./setInputSettings";
+export { default as toFlexRegex } from "./toFlexRegex";

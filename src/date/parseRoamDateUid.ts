f88bf28dd2654b87196d3fff583756380bfe6e2c
David Vargas <dvargas92495@gmail.com>
2022-06-11 14:10:38 -0400

    test failure actually caught

--- src/date/parseRoamDateUid.ts
+++ src/date/parseRoamDateUid.ts
@@ -1,6 +1,6 @@
 const parseRoamDateUid = (s: string): Date => {
   const [month, date, year] = s.split("-").map((s) => Number(s));
-  return new Date(year, month, date);
+  return new Date(year, month - 1, date);
 };
 
 export default parseRoamDateUid;

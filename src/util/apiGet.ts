41ec5970d002c6b16daff64507f60ffc9104a254
David Vargas <dvargas92495@gmail.com>
2022-06-12 15:02:38 -0400

    cleaning up some deps, extending APIGet

--- src/util/apiGet.ts
+++ src/util/apiGet.ts
@@ -1,12 +1,31 @@
 import getAuthorizationHeader from "../util/getAuthorizationHeader";
 
 const apiGet = <T extends Record<string, unknown> = Record<string, never>>(
-  args: string | { path: string; anonymous?: boolean }
+  args:
+    | string
+    | {
+        path: string;
+        anonymous?: boolean;
+        authorization?: string;
+        params?: Record<string, string>;
+      }
 ) => {
   const path = typeof args === "string" ? args : args.path;
   const anonymous = typeof args === "string" ? false : args.anonymous;
-  return fetch(`${process.env.API_URL}/${path}`, {
-    headers: anonymous ? { Authorization: getAuthorizationHeader() } : {},
+  const Authorization =
+    typeof args !== "string" && args.authorization
+      ? args.authorization
+      : getAuthorizationHeader();
+  const params = typeof args !== "string" && args.params ? args.params : {};
+
+  const url = new URL(`${process.env.API_URL}/${path}`);
+  if (process.env.NODE_ENV === "development") {
+    url.searchParams.set("dev", "true");
+  }
+  Object.entries(params).forEach(([k, v]) => url.searchParams.set(k, v));
+
+  return fetch(url, {
+    headers: anonymous ? { Authorization } : {},
   }).then((r) => {
     if (!r.ok) {
       return r.text().then((e) => Promise.reject(new Error(e)));

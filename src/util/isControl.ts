8bb1cb5ed565ed07196aa00213f677d2b207111c
David Vargas <dvargas92495@gmail.com>
2022-06-11 22:12:04 -0400

    no more uacontrol

--- src/util/isControl.ts
+++ src/util/isControl.ts
@@ -1,34 +1,4 @@
-import parser from "ua-parser-js";
-
-const UA = new parser();
-const os = UA.getOS();
-
-const getNavigatorInstance = () => {
-  if (typeof window !== "undefined") {
-    if (window.navigator || navigator) {
-      return window.navigator || navigator;
-    }
-  }
-
-  return false;
-};
-
-const isIOS13Check = () => {
-  const type = "iPad";
-  const nav = getNavigatorInstance();
-  return (
-    nav &&
-    nav.platform &&
-    (nav.platform.indexOf(type) !== -1 ||
-      (nav.platform === "MacIntel" && nav.maxTouchPoints > 1))
-  );
-};
-
-const isIOS = os.name === "iOS" || isIOS13Check();
-const isMacOs = os.name === "Mac OS";
-const isApple = isIOS || isMacOs;
-
 const isControl = (e: KeyboardEvent | MouseEvent): boolean =>
-  (e.ctrlKey && !isApple) || (e.metaKey && isApple);
+  (e.ctrlKey && !window.roamAlphaAPI.platform.isIOS) || (e.metaKey && window.roamAlphaAPI.platform.isIOS);
 
 export default isControl;

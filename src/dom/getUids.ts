33cc6ed5dae3c023a01ed59d0922d164f6232bdf
David Vargas <dvargas92495@gmail.com>
2022-06-16 17:38:38 -0400

    fix getUids

--- src/dom/getUids.ts
+++ src/dom/getUids.ts
@@ -3,7 +3,7 @@ import getUidsFromId from "./getUidsFromId";
 const getUids = (
   block: HTMLDivElement | HTMLTextAreaElement
 ): ReturnType<typeof getUidsFromId> => {
-  return block ? getUidsFromId(block.id) : { blockUid: "", parentUid: "" };
+  return block ? getUidsFromId(block.id) : { blockUid: "", windowId: "" };
 };
 
 export default getUids;

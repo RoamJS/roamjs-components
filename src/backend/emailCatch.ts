e6ca792be8db78548c01ad8bc4adf9879c3fd963
David Vargas <dvargas92495@gmail.com>
2022-07-03 14:02:21 -0400

    add extra handling around 400 errors in email catch

--- src/backend/emailCatch.ts
+++ src/backend/emailCatch.ts
@@ -4,11 +4,20 @@ import headers from "./headers";
 
 const emailCatch =
   (subject: string) =>
-  (e: Error): Promise<APIGatewayProxyResult> =>
-    emailError(subject, e).then((id) => ({
+  (e: Error): Promise<APIGatewayProxyResult> => {
+    const errorCode = Number(e.name);
+    if (errorCode >= 400 && errorCode < 500) {
+      return Promise.resolve({
+        statusCode: errorCode,
+        body: e.message,
+        headers,
+      });
+    }
+    return emailError(subject, e).then((id) => ({
       statusCode: 500,
       body: `Unknown error - Message Id ${id}`,
       headers,
     }));
+  };
 
 export default emailCatch;

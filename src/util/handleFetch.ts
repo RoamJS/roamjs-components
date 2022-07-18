5b0eaadb2ea003b8766936009228bf592dd26a5a
David Vargas <dvargas92495@gmail.com>
2022-07-18 00:05:48 -0400

    handle fetch handling

--- src/util/handleFetch.ts
+++ src/util/handleFetch.ts
@@ -35,8 +35,13 @@ const handleFetch = <T extends Record<string, unknown> = Record<string, never>>(
   ).then((r) => {
     if (!r.ok) {
       return r.text().then((e) => Promise.reject(new Error(e)));
+    } else if (r.status === 204) {
+      return {};
     }
-    return r.json().then((r) => r as T);
+    return r
+      .json()
+      .then((r) => r as T)
+      .catch(() => r.text());
   });
 };
 

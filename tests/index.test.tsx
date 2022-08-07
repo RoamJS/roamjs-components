47c1950f881ff8deac95307428008846536392ff
David Vargas <dvargas92495@gmail.com>
2022-08-07 09:29:17 -0400

    fix test

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -4,9 +4,10 @@ import { createConfigObserver } from "../src/components/ConfigPage";
 
 test("Renders Package", () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore Not setting this whole object get out. Might one day build roam-jest
+  // @ts-ignore Not setting this whole object. Might one day build roam-jest
   window.roamAlphaAPI = {};
   window.roamAlphaAPI.q = () => [[{ uid: "exists" }]];
+  window.roamAlphaAPI.pull = () => ({});
   render(<div className="roam-body" />);
   createConfigObserver({ title: "roam/js/test", config: { tabs: [] } });
 });

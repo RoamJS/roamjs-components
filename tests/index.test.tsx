85e501ecd3ca6a321fc4743bad392bdc4a41ee19
David Vargas <dvargas92495@gmail.com>
2022-08-07 09:41:54 -0400

    fix tests again

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -8,6 +8,11 @@ test("Renders Package", () => {
   window.roamAlphaAPI = {};
   window.roamAlphaAPI.q = () => [[{ uid: "exists" }]];
   window.roamAlphaAPI.pull = () => ({});
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore Not setting this whole object. Might one day build roam-jest
+  window.roamAlphaAPI.util = {
+    generateUID: () => "abcdefghi"
+  }
   render(<div className="roam-body" />);
   createConfigObserver({ title: "roam/js/test", config: { tabs: [] } });
 });

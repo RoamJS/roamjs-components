4e0aec1c3873cbaf1872cf5c99a9ca8b2f77522f
David Vargas <dvargas92495@gmail.com>
2022-08-30 15:39:28 -0400

    improve perf of getParentUidsOfBlockUid

--- src/queries/getParentUidsOfBlockUid.ts
+++ src/queries/getParentUidsOfBlockUid.ts
@@ -1,8 +1,10 @@
+import type { PullBlock } from "../types";
+
 const getParentUidsOfBlockUid = (uid: string): string[] =>
-  window.roamAlphaAPI
-    .q(
-      `[:find ?u :where [?p :block/uid ?u] [?b :block/parents ?p] [?b :block/uid "${uid}"]]`
-    )
-    .map((r) => r[0] as string);
+  (
+    window.roamAlphaAPI.data.fast.q(
+      `[:find (pull ?p [:block/uid]) :where [?b :block/uid "${uid}"] [?b :block/parents ?p] ]`
+    ) as [PullBlock][]
+  ).map((r) => r[0][":block/uid"] || "");
 
 export default getParentUidsOfBlockUid;

fb74fb1996935a698fb48217c7d73589d0544de4
David Vargas <dvargas92495@gmail.com>
2022-09-08 16:51:17 -0400

    adding a before param to renderOverlay

--- src/util/renderOverlay.ts
+++ src/util/renderOverlay.ts
@@ -16,18 +16,26 @@ const renderOverlay = <T extends Record<string, unknown>>({
   Overlay = (props) => React.createElement("div", props),
   props = {} as T,
   path = "body",
+  before,
 }: {
   id?: string;
   Overlay?: (props: RoamOverlayProps<T>) => React.ReactElement;
   props?: T;
   path?: string | HTMLElement | null;
+  before?: number | HTMLElement | null;
 } = {}): (() => void) | void => {
   const parent = document.createElement("div");
   parent.id = id;
   const pathElement =
     typeof path === "string" ? document.querySelector(path) : path;
   if (pathElement && !pathElement.querySelector(`#${id}`)) {
-    pathElement.appendChild(parent);
+    if (typeof before === "number") {
+      pathElement.insertBefore(parent, pathElement.children[before]);
+    } else if (typeof before === "object") {
+      pathElement.insertBefore(parent, before);
+    } else {
+      pathElement.appendChild(parent);
+    }
     const onClose = () => {
       if (typeof props.onClose === "function") props.onClose();
       ReactDOM.unmountComponentAtNode(parent);

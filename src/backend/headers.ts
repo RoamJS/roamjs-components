384e9425004639172975acd3a9d6e024ad2de347
David Vargas <dvargas92495@gmail.com>
2023-07-15 16:01:51 -0400

    fix testing library dep hell and backend rmd

--- src/backend/headers.ts
+++
@@ -1,6 +0,0 @@
-const headers = {
-  "Access-Control-Allow-Origin": "https://roamresearch.com",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-};
-
-export default headers;
--- src/backend/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as emailCatch } from "./emailCatch";
-export { default as emailError } from "./emailError";
-export { default as getRoamJSUser, awsGetRoamJSUser } from "./getRoamJSUser";
-export { default as headers } from "./headers";
-export { default as meterRoamJSUser } from "./meterRoamJSUser";
-export { default as putRoamJSUser, awsPutRoamJSUser } from "./putRoamJSUser";

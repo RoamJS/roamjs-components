66d258f7d2039c0ecdd0fb23a2ad35717f0bcab6
Michael Gartner <mclicks@gmail.com>
2023-09-21 12:52:39 -0600

    return "" if title is ""

--- src/queries/getPageUidByPageTitle.ts
+++ src/queries/getPageUidByPageTitle.ts
@@ -1,6 +1,10 @@
-const getPageUidByPageTitle = (title: string): string =>
-  window.roamAlphaAPI.pull("[:block/uid]", [":node/title", title])?.[
-    ":block/uid"
-  ] || "";
+const getPageUidByPageTitle = (title: string): string => {
+  if (!title) return "";
+  return (
+    window.roamAlphaAPI.pull("[:block/uid]", [":node/title", title])?.[
+      ":block/uid"
+    ] || ""
+  );
+};
 
 export default getPageUidByPageTitle;

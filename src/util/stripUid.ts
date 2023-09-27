31822fa82647abb2e71eceea792188c2d57c1b1e
Michael Gartner <mclicks@gmail.com>
2023-09-27 10:58:17 -0600

    fix lint

--- src/util/stripUid.ts
+++ src/util/stripUid.ts
@@ -1,7 +1,7 @@
 import { InputTextNode, RoamBasicNode } from "../types";
 
 export const stripUid = (n: RoamBasicNode[]): InputTextNode[] => {
-  return n.map(({ uid, children, ...c }) => ({
+  return n.map(({ uid: _uid, children, ...c }) => ({
     ...c,
     children: stripUid(children),
   }));

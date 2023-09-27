9e13f8da906b35b3d1704d82e43fd88509f83a16
Michael Gartner <mclicks@gmail.com>
2023-09-27 11:00:54 -0600

    export default

--- src/util/stripUid.ts
+++ src/util/stripUid.ts
@@ -1,8 +1,10 @@
 import { InputTextNode, RoamBasicNode } from "../types";
 
-export const stripUid = (n: RoamBasicNode[]): InputTextNode[] => {
+const stripUid = (n: RoamBasicNode[]): InputTextNode[] => {
   return n.map(({ uid: _uid, children, ...c }) => ({
     ...c,
     children: stripUid(children),
   }));
 };
+
+export default stripUid;

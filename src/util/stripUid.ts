2eaa55fe45a383afd9435690d60010d64e49629d
Michael Gartner <mclicks@gmail.com>
2023-09-27 10:55:44 -0600

    add stripUid

---
+++ src/util/stripUid.ts
@@ -0,0 +1,8 @@
+import { InputTextNode, RoamBasicNode } from "../types";
+
+export const stripUid = (n: RoamBasicNode[]): InputTextNode[] => {
+  return n.map(({ uid, children, ...c }) => ({
+    ...c,
+    children: stripUid(children),
+  }));
+};

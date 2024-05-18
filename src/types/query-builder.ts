b9cc0b7997625420f8e457dc9d153213edb0c768
Michael Gartner <mclicks@gmail.com>
2024-05-18 12:50:00 -0600

    add missing types

--- src/types/query-builder.ts
+++ src/types/query-builder.ts
@@ -5,4 +5,9 @@ export type Result = { text: string; uid: string } & Record<
   Record<string, string | number | Date>;
 
 export type RunQuery = (parentUid: string) => Promise<Result[]>;
+export type RunQuerySync = (parentUid: string) => Result[];
 export type ListActiveQueries = () => { uid: string }[];
+export type IsDiscourseNode = (
+  uid?: string | undefined,
+  nodes?: any
+) => boolean;

41b3c79711ec5e3d2b23adcc7534a2aa2caf5f66
Michael Gartner <mclicks@gmail.com>
2025-03-13 14:14:40 -0600

    Refactor MenuItemSelect component and added support for children as a… (#39)
    * Refactor MenuItemSelect component and added support for children as a render prop
    
    * 0.85.0

--- src/components/MenuItemSelect.tsx
+++ src/components/MenuItemSelect.tsx
@@ -1,54 +1,86 @@
-import { Button, IButtonProps, MenuItem } from "@blueprintjs/core";
-import { SelectProps, Select } from "@blueprintjs/select";
-import React, { ReactText } from "react";
+import { Button, ButtonProps, MenuItem } from "@blueprintjs/core";
+import { SelectProps, Select, ICreateNewItem } from "@blueprintjs/select";
+import React, { ReactText, ButtonHTMLAttributes, ReactNode } from "react";
+
+type ActiveItem<T> = T | ICreateNewItem | null | undefined;
+type TransformItem<T> = (s: T) => ReactNode;
+type MenuItemSelectProps<T extends ReactText> = Omit<
+  SelectProps<T>,
+  "itemRenderer"
+> & {
+  ButtonProps?: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>;
+  emptyValueText?: string;
+  transformItem?: TransformItem<T>;
+  children?: (props: {
+    activeItem: ActiveItem<T>;
+    emptyValueText?: string;
+    transformItem?: TransformItem<T>;
+  }) => ReactNode;
+};
 
 const MenuItemSelect = <T extends ReactText>(
-  props: Omit<SelectProps<T>, "itemRenderer"> & {
-    ButtonProps?: IButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
-  } & { emptyValueText?: string; transformItem?: (s: T) => React.ReactNode }
+  props: MenuItemSelectProps<T>
 ): JSX.Element => {
   const TypeSelect = Select.ofType<T>();
   const itemPredicate = (query: string, item: T) => {
     const text = props.transformItem ? props.transformItem(item) : item;
     return String(text).toLowerCase().includes(query.toLowerCase());
   };
-  const { activeItem, filterable = false, ...selectProps } = props;
+  const {
+    activeItem,
+    filterable = false,
+    children,
+    transformItem,
+    emptyValueText,
+    ...selectProps
+  } = props;
+
+  const defaultButton = (
+    <Button
+      text={
+        activeItem ? (
+          transformItem ? (
+            transformItem(activeItem as T)
+          ) : (
+            activeItem
+          )
+        ) : (
+          <span className="opacity-50 italic">
+            {emptyValueText || "Choose..."}
+          </span>
+        )
+      }
+      rightIcon="double-caret-vertical"
+      {...props.ButtonProps}
+    />
+  );
+
   return (
     <TypeSelect
       {...selectProps}
       itemRenderer={(item, { modifiers, handleClick }) => (
         <MenuItem
           key={item}
-          text={props.transformItem ? props.transformItem(item) : item}
+          text={transformItem ? transformItem(item) : item}
           active={modifiers.active}
           onClick={handleClick}
         />
       )}
       filterable={filterable}
-      itemPredicate={props.filterable ? itemPredicate : undefined}
+      itemPredicate={filterable ? itemPredicate : undefined}
       popoverProps={{
         minimal: true,
         captureDismiss: true,
         ...props.popoverProps,
       }}
     >
-      <Button
-        text={
-          activeItem ? (
-            props.transformItem ? (
-              props.transformItem(activeItem as T)
-            ) : (
-              activeItem
-            )
-          ) : (
-            <i style={{ opacity: 0.5 }}>
-              {props.emptyValueText || "Choose..."}
-            </i>
-          )
-        }
-        rightIcon="double-caret-vertical"
-        {...props.ButtonProps}
-      />
+      {children
+        ? children({
+            activeItem,
+            emptyValueText,
+            transformItem,
+          })
+        : defaultButton}
     </TypeSelect>
   );
 };
